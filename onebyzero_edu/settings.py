14b3e06b4761603bd0f7ac1529ec0e9d97aec718
obaydulnayeem <nayeem.cse6.bu@gmail.com>
2024-06-06 23:31:31 +0600

    brand name modified

--- onebyzero_edu/settings.py
+++ onebyzero_edu/settings.py
@@ -100,25 +100,25 @@ WSGI_APPLICATION = 'onebyzero_edu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'onebyze1_onebyzero_edu_four',
-        'USER': 'onebyze1_nayeem1',
-        'PASSWORD': '*0HN#@nayeem1.$',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'onebyze1_onebyzero_edu_four',
+#         'USER': 'onebyze1_nayeem1',
+#         'PASSWORD': '*0HN#@nayeem1.$',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # DATABASES = {
